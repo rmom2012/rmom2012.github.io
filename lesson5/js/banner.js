dc5c213339635ab834f713912c5b7071703519d9
Rmom2012 <mca12005@byui.edu>
2020-02-08 23:18:04 -0800

    initial commit

---
+++ lesson5/js/banner.js
@@ -0,0 +1,2 @@
+new Date().getDay == 5 ?
+document.getElementById('banner').innerHTML = "Saturday = Preston Pancakes in the Park! 9:00 a.m. Saturday at the city park pavilion." : banner1.style.display = "none";
