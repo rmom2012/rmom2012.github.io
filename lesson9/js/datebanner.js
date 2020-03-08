554b915a6f0a7b78bb1e2dc028dbf4d12ab2797b
Rmom2012 <mca12005@byui.edu>
2020-03-07 22:12:59 -0800

    initial commit

---
+++ lesson9/js/datebanner.js
@@ -0,0 +1,9 @@
+
+var date = new Date();
+document.getElementById("year").textContent = date.getFullYear();
+
+var month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+var day = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+document.getElementById("date").textContent = day[date.getDay()] + ", " + date.getDate() + " " + month[date.getMonth()] + " " + date.getFullYear();
+
