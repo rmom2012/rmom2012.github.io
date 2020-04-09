0629ef31ae4bce8843d150a147e976e9f931d13b
Rmom2012 <mca12005@byui.edu>
2020-04-08 18:31:21 -0700

    5th commit

---
+++ Salmon River/scripts/guides.js
@@ -0,0 +1,42 @@
+const requestURL = 'https://rmom2012.github.io/Salmon%20River/riverGuide.json';
+
+fetch(requestURL)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (jsonObject) {
+    
+
+  const guides = jsonObject['guides'];
+  for (let i = 0; i < guides.length; i++ ) {
+      let card = document.createElement ('section');
+      let name = document.createElement ('h2');
+      let imageurl = document.createElement ('img');
+      let certification = document.createElement ('p');
+      let experience = document.createElement ('p');
+      let email = document.createElement ('p');
+      let bio = document.createElement ('p');
+      
+
+name.textContent = guides[i].name + '' + guides[i].lastname;
+card.appendChild(name);
+
+imageurl.setAttribute( 'src', guides[i].imageurl);
+imageurl.setAttribute('alt' , guides[i].name);
+card.appendChild(imageurl);
+
+certification.textContent = 'Certification:' + guides[i].certification;
+card.appendChild(certification);
+
+experience.textContent = 'Experience:' + guides[i].experience;
+card.appendChild(experience);
+
+email.textContent = 'Email:' + guides[i].email;
+card.appendChild(email);
+
+bio.textContent = 'Biography:' + guides[i].bio;
+card.appendChild(bio);
+
+document.querySelector('div.cards').appendChild(card);}
+  });
+
